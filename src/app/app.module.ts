a79d007c7eae7d2064fb75f5e830fd739ef60391
florian.schafer <florian.schafer@outlook.com>
2020-06-14 21:38:45 +0200

    Adding security

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { DummyPageComponent } from './dummy-page/dummy-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DummyPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/security/auth.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from "@angular/core";
+import { Observable, ReplaySubject } from "rxjs";
+import { AuthResponse } from "../models/auth-response";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { User } from "../models/user";
+import { AuthRequest } from "../models/auth-request";
+
+// TODO: Insert here your personnal api URL
+const apiUrl = "https://masrad-2020-tl-florian.herokuapp.com/api";
+
+const STORAGE_KEY = "auth";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+  /**
+   * A "ReplaySubject" is a Subject (a source of an Observable) that emits a predefined number of previously emitted
+   * values to an Observer when it subscribes to it.
+   * It will act as a sort of local "cache" for the AuthResponse object value.
+   */
+  private authenticated$: ReplaySubject<AuthResponse>;
+
+  constructor(private http: HttpClient) {
+    // Get the credentials from the localStorage when the AuthService is created
+    // It will either contains an AuthResponse object of null if it does not exist
+    // const savedAuth = JSON.parse(
+    //   localStorage.getItem(STORAGE_KEY)
+    // ) as AuthResponse;
+    // Create the ReplaySubject and configure it so that it emits the latest emitted value on each subscription
+    this.authenticated$ = new ReplaySubject(1);
+    // Emit a null value as the initial value
+    this.authenticated$.next(null);
+  }
+
+  /**
+   * Checks if the user is authenticated by casting the latest AuthResponse value as a boolean
+   */
+  isAuthenticated(): Observable<boolean> {
+    return this.authenticated$.pipe(map((auth) => Boolean(auth)));
+  }
+
+  /**
+   * Retrieves the User object from the latest AuthResponse value
+   */
+  getUser(): Observable<User> {
+    return this.authenticated$.pipe(
+      map((auth) => (auth ? auth.user : undefined))
+    );
+  }
+
+  /**
+   * Retrieves the token string from the latest AuthResponse value
+   */
+  getToken(): Observable<string> {
+    return this.authenticated$.pipe(
+      map((auth) => (auth ? auth.token : undefined))
+    );
+  }
+
+  /**
+   * Logs in a user with the provided AuthRequest object and emits the received AuthResponse if successful.
+   */
+  login(authRequest: AuthRequest): Observable<User> {
+    return this.http.post<AuthResponse>(`${apiUrl}/auth`, authRequest).pipe(
+      map((response) => {
+        this.authenticated$.next(response);
+        console.log(`User ${response.user.name} logged in`);
+        return response.user;
+      })
+    );
+  }
+
+  /**
+   * Logs out a user and emit an empty AuthResponse
+   */
+  logout() {
+    this.authenticated$.next(null);
+    console.log("User logged out");
+  }
+}
